b4a478786b3bed7971cc8fb1eda9e256ae8812d5
Keerthana Sriram <keerthanasreeraam@gmail.com>
2025-09-22 23:07:33 +0530

    Phase 2: Added Doctor and Appointment tables + DAOs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,16 @@ package com.pms;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Patient Management System Backend is running!");
+          try {
+            Connection conn = DBConnection.getConnection();
+            PatientDAO patientDAO = new PatientDAO(conn);
+
+            // Test insert
+            patientDAO.addPatient("John Doe", 30, "Male", "9876543210", "No allergies");
+
+            System.out.println("Patient inserted successfully!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
